4622848f73ebee457c7480c9164d178d3647abeb
lsze5821 <lsze5821@H202R6S4.STUDENT.ad.fgcu.edu>
2019-02-12 16:03:50 -0500

    New Changes

---
+++ src/Tests1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Tests1 {
+    public static void main(String args[]) {
+    	  String greet1 = "Hello";
+    	  String greet2 = "World";
+    	  String message2 = greet1 +" " +greet2 +" " +2016 +"!";
+    	  System.out.println(message2);
+    	}
+  
+        
+    }
+
