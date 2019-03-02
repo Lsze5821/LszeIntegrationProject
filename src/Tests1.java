331f55317fc0d5427cd2b12412a59ebda84d8051
Lsze5821 <46796206+Lsze5821@users.noreply.github.com>
2019-03-01 23:23:09 -0500

    Added more comments

--- src/Tests1.java
+++ src/Tests1.java
@@ -19,7 +19,7 @@ public class Tests1 {
       System.out.println("Not Weird");
     } else if (2 <= N && N <= 5 && N % 2 == 0) {// if else construct
       System.out.println("Not Weird");
-    } else if (6 <= N && N <= 20 && N % 2 == 0) {
+    } else if (6 <= N && N <= 20 && N % 2 == 0) {//even statement using divisible by 2 is equal to 0
       System.out.println("Weird");
     } else if (N % 2 == 1) {
       System.out.println("Weird");
